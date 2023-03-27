14c9dc2eea99142e194f39ced534d65001f88b0c
Matthews Brito <109427217+Matthewsfx05@users.noreply.github.com>
2023-03-27 12:34:22 -0300

    Add files via upload

---
+++ ATV ARRAY/script.js
@@ -0,0 +1,13 @@
+const input = document.getElementById('input')
+const adicionar = documnet.getElementById('adicionar')
+const ordenar = doment.getElementById('ordernar')
+let array = []
+
+adicionar.addEventListener('click', function(){
+    array.push(input.value);
+    console.log(array)
+
+     if(input.value); {
+        alert("informe um valor válido")
+     }
+})
